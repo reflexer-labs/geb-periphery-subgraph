b23290de0d6d02efebe6bbd1c9fed69828a0478b
Guillaume Felley <guillaume@felley.io>
2021-07-08 11:05:01 +0200

    Savior fix

--- src/mappings/savior-uniswap-lp.ts
+++ src/mappings/savior-uniswap-lp.ts
@@ -8,14 +8,19 @@ import {
 import { SaviorBalance, SaviorBalanceChange } from '../../generated/schema'
 
 import * as decimal from '../utils/decimal'
+import { eventUid } from '../utils/ethereum'
 
 export function handleSaveSAFE(event: SaveSAFE): void {
   let balance = getOrCreateSaviorBalance(event.address, event.params.safeHandler, event)
-  let change = getOrCreateSaviorBalanceChange(event.address, event.params.safeHandler, event)
 
+  let change = new SaviorBalanceChange(eventUid(event))
+  change.saviorAddress = event.address
+  change.address = event.params.safeHandler
   // The balance is wiped
-
   change.deltaBalance = balance.balance.neg()
+  change.createdAt = event.block.timestamp
+  change.createdAtBlock = event.block.number
+  change.createdAtTransaction = event.transaction.hash
   change.save()
 
   balance.balance = decimal.ZERO
@@ -27,8 +32,13 @@ export function handleDeposit(event: Deposit): void {
   balance.balance = balance.balance.plus(decimal.fromWad(event.params.lpTokenAmount))
   balance.save()
 
-  let change = getOrCreateSaviorBalanceChange(event.address, event.params.safeHandler, event)
+  let change = new SaviorBalanceChange(eventUid(event))
+  change.saviorAddress = event.address
+  change.address = event.params.safeHandler
   change.deltaBalance = decimal.fromWad(event.params.lpTokenAmount)
+  change.createdAt = event.block.timestamp
+  change.createdAtBlock = event.block.number
+  change.createdAtTransaction = event.transaction.hash
   change.save()
 }
 
@@ -37,8 +47,13 @@ export function handleWithdraw(event: Withdraw): void {
   balance.balance = balance.balance.minus(decimal.fromWad(event.params.lpTokenAmount))
   balance.save()
 
-  let change = getOrCreateSaviorBalanceChange(event.address, event.params.safeHandler, event)
+  let change = new SaviorBalanceChange(eventUid(event))
+  change.saviorAddress = event.address
+  change.address = event.params.safeHandler
   change.deltaBalance = decimal.fromWad(event.params.lpTokenAmount).neg()
+  change.createdAt = event.block.timestamp
+  change.createdAtBlock = event.block.number
+  change.createdAtTransaction = event.transaction.hash
   change.save()
 }
 
@@ -61,27 +76,6 @@ function getOrCreateSaviorBalance(
   }
   balance.save()
   return balance as SaviorBalance
-}
-
-function getOrCreateSaviorBalanceChange(
-  savior: Address,
-  safeHandler: Address,
-  event: ethereum.Event,
-): SaviorBalanceChange {
-  let id = saviorId(savior, safeHandler)
-  let change = SaviorBalanceChange.load(id)
-  if (change == null) {
-    change = new SaviorBalanceChange(id)
-    change.saviorAddress = savior
-    change.address = safeHandler
-    change.deltaBalance = decimal.ZERO
-
-    change.createdAt = event.block.timestamp
-    change.createdAtBlock = event.block.number
-    change.createdAtTransaction = event.transaction.hash
-  }
-  change.save()
-  return change as SaviorBalanceChange
 }
 
 export function saviorId(savior: Address, safeHandler: Address): string {
